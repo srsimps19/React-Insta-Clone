c09539a3c516a75f15104688971679349244ed67
Sydney Crumley <srsimps19@gmail.com>
2019-04-29 18:07:04 -0500

    styled comment section

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './CommentSection.scss';
 
 function CommentSection(props) {
     return(
         <>
             {props.posts.map(posts => (
                 <div key={posts.comments} className="comments">
-                    {posts.comments && posts.comments.map((e,i) => <p key={i}><h4>{e.username}</h4>{e.text}</p>)}
+                    {posts.comments.map((e,i) => <p key={i}><h4>{e.username}</h4><p>{e.text}</p></p>)}
                 </div>
             ))}
         </>
