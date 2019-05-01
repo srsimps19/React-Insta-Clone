648eb3175e5843f95638c5ef8f1ecbf6947dcf3a
Sydney Crumley <srsimps19@gmail.com>
2019-04-30 22:06:01 -0500

    refactored post container and added post.js

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,42 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import CommentSection from '../CommentSection/CommentSection';
-import './PostContainer.scss'
+import Post from './Post';
+import'./PostContainer.scss'
 
-function PostContainer(props) {
-    return(
-        <>
-            {props.posts.map((post,i) => (
-                <div key={i} className="posts">
-                    <div className="postHeader">
-                        <img src={post.thumbnailUrl} alt={post.username} className="thumbnail"/>
-                        <h4>{post.username}</h4>
-                    </div>
-                    <img src={post.imageUrl} alt="post" className="postImage" />
-                    <div className="belowPicture">
-                        <i className="far fa-heart fa-lg"></i>
-                        <i className="far fa-comment fa-lg"></i>
-                        <h4>{post.likes} likes</h4>
-                    </div>
-                    <CommentSection comments={post.comments} handleChanges={props.handleChanges} addNewComment={props.addNewComment} postIndex={i} />
-                    <div className="timestamp">
-                        {post.timestamp}
-                    </div>
-                </div>
-            ))}
-        </>
-    );
-}
-
-PostContainer.propTypes = {
-    posts: PropTypes.arrayOf(
-        PropTypes.shape({
-            username: PropTypes.string.isRequired,
-            thumbnailUrl: PropTypes.string.isRequired,
-            imageUrl: PropTypes.string.isRequired,
-            likes: PropTypes.number.isRequired,
-        })
-    ).isRequired
+const PostContainer = props => {
+    return (
+        <div className="posts-container-wrapper">
+          {props.posts.map(p => <Post key={p.imageUrl} post={p} />)}
+        </div>
+      );
 }
 
 export default PostContainer;
