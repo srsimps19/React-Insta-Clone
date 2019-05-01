1ed7fb93a60eabfbc4a7a4b55816820c014e7afd
Sydney Crumley <srsimps19@gmail.com>
2019-05-01 15:22:29 -0500

    updated withAuthenticate to accept 2 components

--- instagram-app/src/components/Authentication/withAuthenticate.js
+++ instagram-app/src/components/Authentication/withAuthenticate.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const withAuthenticate = Comp => 
+const withAuthenticate = Comp1 => Comp2 =>
     class extends React.Component {
         render() {
-            return <Comp />;
+            if (localStorage.getItem('user')) {
+                return <Comp1 />;
+            } else {
+                return <Comp2 />
+            }
         }
     };
 export default withAuthenticate;
