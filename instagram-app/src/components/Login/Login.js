1a957010501d583b2696d3eb785ed0c277ea6beb
Sydney Crumley <srsimps19@gmail.com>
2019-05-01 18:00:31 -0500

    added h1 to login page and restyled login page

--- instagram-app/src/components/Login/Login.js
+++ instagram-app/src/components/Login/Login.js
@@ -25,19 +25,23 @@ class Login extends React.Component {
     render() {
         return (
             <div className='loginPage'>
-                <Form inline onSubmit={this.handleLogin} className="loginForm">
-                    <FormGroup>
-                    <Label for="username" hidden>Username</Label>
-                    <Input type="text" name="username" id="username" placeholder="Username" value={this.state.username} onChange={this.handleInputChanges} />
-                    </FormGroup>
-                    {' '}
-                    <FormGroup>
-                    <Label for="examplePassword" hidden>Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="Password" value={this.state.password} onChange={this.handleInputChanges} />
-                    </FormGroup>
-                    {' '}
-                    <Button>Submit</Button>
-                </Form>
+              <div className="loginHeading">
+                <i className="fab fa-instagram fa-3x"></i>
+                <h1>React Instagram Clone</h1>
+              </div>
+              <Form inline onSubmit={this.handleLogin} className="loginForm">
+                <FormGroup>
+                  <Label for="username" hidden>Username</Label>
+                  <Input type="text" name="username" id="username" placeholder="Username" value={this.state.username} onChange={this.handleInputChanges} />
+                </FormGroup>
+                  {' '}
+                <FormGroup>
+                  <Label for="examplePassword" hidden>Password</Label>
+                  <Input type="password" name="password" id="examplePassword" placeholder="Password" value={this.state.password} onChange={this.handleInputChanges} />
+                </FormGroup>
+                  {' '}
+                  <Button>Submit</Button>
+              </Form>
           </div>
         );
       }
