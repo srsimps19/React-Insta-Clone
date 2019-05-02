98951a82423c36a6ca0477fa15658c80f6fd4056
Sydney Crumley <srsimps19@gmail.com>
2019-05-02 14:55:18 -0500

    refactored search bar to include styled components

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,23 +1,72 @@
 import React from 'react';
-import './SearchBar.scss';
+import styled from 'styled-components';
+
+
+export const Header = styled.header`
+  width: 900px;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  margin: 15px 0;
+  border-radius: 6px;
+  border: 1px solid grey;
+  background-color: white;`;
+
+export const LeftSide = styled.div`
+  display: flex;
+
+  .fa-instagram {
+    padding-top: 10px;
+    padding-left: 10px;
+  }
+
+  p {
+    margin: 0;
+    padding: 0 15px;
+    font-size: 2.5rem;
+    color: grey;
+  }
+
+  h2 {
+    font-size: 2.5rem;
+    font-family: 'Satisfy', cursive;
+    padding-top: 15px;
+    padding-right: 10px;
+  }`;
+
+export const Center = styled.div`
+  input {
+  width: 275px;
+  height: 40px;
+  text-align: center;
+  margin: 20px 10px 10px 10px;
+  border-radius: 6px;
+  border: 1px solid grey;
+  }`;
+
+export const RightSide = styled.div`
+  margin-right: 10px;
+    .fa-heart {
+        padding: 10px 20px;
+    }`;
 
 const SearchBar = props => {
   return (
-    <div className="searchBar">
-      <div className="left-side">
+    <Header>
+      <LeftSide>
         <i className="fab fa-instagram fa-3x"></i>
         <p>|</p>
         <h2>Instagram</h2>
-      </div>
-      <div className="center">
+      </LeftSide>
+      <Center>
         <input type="text" placeholder="🔍 Search" onChange={props.searchPosts} />
-      </div>
-      <div className="right-side">
+      </Center>
+      <RightSide>
           <i className="far fa-compass fa-3x" />
           <i className="far fa-heart fa-3x" />
           <i className="far fa-user-circle fa-3x" />
-      </div>
-    </div>
+      </RightSide>
+    </Header>
   );
 };
 
